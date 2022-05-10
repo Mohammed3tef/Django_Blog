738912347652047af2c2623df47dcda9f938c2c6
mostafa shawky <94018781+Mostafa-Shawky2021@users.noreply.github.com>
2022-05-11 01:46:52 +0200

    add route delete post

--- Dj_blog/posts/urls.py
+++ Dj_blog/posts/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.posts),
     path('createpost', views.post_create),
     path('updatepost/<id>', views.post_update),
+    path('delpost/<num>',views.post_delete),  
 ]
