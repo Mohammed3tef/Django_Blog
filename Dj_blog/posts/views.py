159468cd203fa46386d90edc2e453d36274daca1
SamarNegm <negmsama890@gmail.com>
2022-05-11 00:12:23 +0200

    view post

--- Dj_blog/posts/views.py
+++ Dj_blog/posts/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from .models import Post, Tag, Category, Comment, Profanity
+from django.db.models import Q
+from django.shortcuts import render, get_object_or_404
+from posts.forms import PostForm, CommentForm
+from users.util_funcs import delete_profile_pic
+from users.logger import log
+from django.core.mail import send_mail
 
 # Create your views here.
+
+
+def posts(request):
+    posts = Post.objects.all()
+    popular_posts = Post.objects.order_by('-likes')[:5]
+    paginator = Paginator(posts, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    categotries = Category.objects.all()
+    tags = Tag.objects.all()[:10]
+    user = request.user
+
+    context = {'page_obj': page_obj, 'categories': categotries,
+               'tags': tags, 'user': user, 'popular_posts': popular_posts}
+    return render(request, 'home.html', context)
