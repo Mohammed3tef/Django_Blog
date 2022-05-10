e41f680e47625f89864f59ba852280e206c4c1d7
SamarNegm <negmsama890@gmail.com>
2022-05-11 00:41:14 +0200

    update post url

--- Dj_blog/posts/urls.py
+++ Dj_blog/posts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from posts import views
 
 urlpatterns = [
-    path('', views.posts),  
-    path('createpost',views.post_create),
-
+    path('', views.posts),
+    path('createpost', views.post_create),
+    path('updatepost/<id>', views.post_update),
 ]
