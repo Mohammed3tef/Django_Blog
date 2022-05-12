fa6aa0e04ce50324ceb5847b277a0ee00dfb9378
Mohamed Sabry <30502846+Mohamedfadah@users.noreply.github.com>
2022-05-12 14:52:22 +0200

    reset password form - update password

--- Dj_blog/users/urls.py
+++ Dj_blog/users/urls.py
@@ -15,7 +15,9 @@ urlpatterns = [
 
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name="registration/password_reset_form.html"), name='password_reset'),
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name="registration/password_reset_done.html"), name='password_reset_done'),
-
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_confirm.html"), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name="registration/password_reset_complete.html"), name='password_reset_complete'),
+    
 
 
 ]
